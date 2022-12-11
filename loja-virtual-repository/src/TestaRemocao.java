5a48ecae2e221794852711b76da2e89f49a18632
Thiago Guerreiro <thiagoguerreiro.si@gmail.com>
2022-12-11 14:42:33 -0300

    Removendo dados

---
+++ loja-virtual-repository/src/TestaRemocao.java
@@ -0,0 +1,22 @@
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TestaRemocao {
+
+	public static void main(String[] args) throws SQLException {
+		
+		ConnectionFactory connectionFactory = new ConnectionFactory();
+		Connection con = connectionFactory.recuperarConexao();
+		
+		Statement stm = con.createStatement();
+		stm.execute("DELETE FROM PRODUTO WHERE ID > 2");
+		
+		Integer linhasModificadas = stm.getUpdateCount();
+		System.out.println("Qtde de linhas excluídas: " + linhasModificadas);
+		
+		con.close();
+
+	}
+
+}
