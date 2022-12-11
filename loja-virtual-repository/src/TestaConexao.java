7c13d66f401c14fe3baeb777db81dc1ab3afefdf
Thiago Guerreiro <thiagoguerreiro.si@gmail.com>
2022-12-11 14:19:05 -0300

    ConnectionFactory

--- loja-virtual-repository/src/TestaConexao.java
+++ loja-virtual-repository/src/TestaConexao.java
@@ -1,15 +1,14 @@
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class TestaConexao {
 
 	public static void main(String[] args) throws SQLException {
 		
-		Connection connection = DriverManager
-				.getConnection("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC", "root", "1234");
+		ConnectionFactory connectionFactory = new ConnectionFactory();
+		Connection con = connectionFactory.recuperarConexao();
 		
-		connection.close();
+		con.close();
 		
 	}
 
--- loja-virtual-repository/src/TestaListagem.java
+++ loja-virtual-repository/src/TestaListagem.java
@@ -1,5 +1,4 @@
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -8,8 +7,8 @@ public class TestaListagem {
 	
 	public static void main(String[] args) throws SQLException {
 		
-		Connection con = DriverManager
-				.getConnection("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC", "root", "1234");
+		ConnectionFactory connectionFactory = new ConnectionFactory();
+		Connection con = connectionFactory.recuperarConexao();
 		
 		Statement stm = con.createStatement();
 		stm.execute("SELECT ID, NOME, DESCRICAO FROM PRODUTO");
