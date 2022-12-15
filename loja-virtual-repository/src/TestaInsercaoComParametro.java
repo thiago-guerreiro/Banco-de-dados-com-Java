a1f41599b097ed59a17f6d834100832ed4603671
Thiago Guerreiro <thiagoguerreiro.si@gmail.com>
2022-12-14 22:56:34 -0300

    Commit e tratamento de exceções com rollback

--- loja-virtual-repository/src/TestaInsercaoComParametro.java
+++ loja-virtual-repository/src/TestaInsercaoComParametro.java
@@ -7,27 +7,37 @@ import java.sql.Statement;
 public class TestaInsercaoComParametro {
 
 	public static void main(String[] args) throws SQLException {
-		
-		String nome = "Mouse";
-		String descricao = "Mouse sem fio";
-		
+				
 		ConnectionFactory connectionFactory = new ConnectionFactory();
 		Connection con = connectionFactory.recuperarConexao();
+		con.setAutoCommit(false);
 		
 		PreparedStatement stm = con.prepareStatement("INSERT INTO PRODUTO (NOME, DESCRICAO) VALUES(?, ?)", Statement.RETURN_GENERATED_KEYS);
 		
+		adicionarVariavel("Smart TV", "Smart TV 45 polegadas", stm);
+		adicionarVariavel("Videogame", "Xbox One", stm);
+		
+		con.commit();
+		
+		stm.close();
+		con.close();
+
+	}
+
+	private static void adicionarVariavel(String nome, String descricao, PreparedStatement stm) throws SQLException {
+		
 		stm.setString(1, nome);
 		stm.setString(2, descricao);
-		
+	
 		stm.execute();
-		
+	
 		ResultSet rst = stm.getGeneratedKeys();
 		while(rst.next()) {
 			Integer id = rst.getInt(1);
 			System.out.println("O id criado foi: " + id);
 		}
-		
-		con.close();
+	
+		rst.close();
 		
 	}
 
