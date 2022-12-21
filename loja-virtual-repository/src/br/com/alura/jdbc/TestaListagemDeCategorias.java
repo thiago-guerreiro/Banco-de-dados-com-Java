4fc9cd173cd1c75c68ad66fa6498d583e0f70a91
Thiago Guerreiro <thiagoguerreiro.si@gmail.com>
2022-12-20 22:42:46 -0300

    Query com inner join

--- loja-virtual-repository/src/br/com/alura/jdbc/TestaListagemDeCategorias.java
+++ loja-virtual-repository/src/br/com/alura/jdbc/TestaListagemDeCategorias.java
@@ -7,17 +7,22 @@ import java.util.List;
 import br.com.alura.jdbc.dao.CategoriaDAO;
 import br.com.alura.jdbc.factory.ConnectionFactory;
 import br.com.alura.jdbc.modelo.Categoria;
+import br.com.alura.jdbc.modelo.Produto;
 
 public class TestaListagemDeCategorias {
 
 	public static void main(String[] args) throws SQLException {
-		
-		try(Connection connection = new ConnectionFactory().recuperarConexao()) {
-			CategoriaDAO categoriaDAO = new CategoriaDAO(connection);
-			List<Categoria> listaDeCategorias = categoriaDAO.listar();
-			listaDeCategorias.stream().forEach(ct -> System.out.println(ct.getNome()));
-		}
 
+		try(Connection connection = new ConnectionFactory().recuperarConexao()) {
+			CategoriaDAO categoriaDAO = new CategoriaDAO(connection);	
+			List<Categoria> listaDeCategorias = categoriaDAO.listarComProduto();
+			listaDeCategorias.stream().forEach(ct -> {
+				System.out.println(ct.getNome());
+				for(Produto produto : ct.getProdutos()) {
+					System.out.println(ct.getNome() + " - " + produto.getNome());
+				}
+			});
+		}
 	}
 
 }
