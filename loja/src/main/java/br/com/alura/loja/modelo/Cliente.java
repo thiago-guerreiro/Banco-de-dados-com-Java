28d15d34505f7a3c1640d345d4cb2af2d548c9c9
Thiago Guerreiro <thiagoguerreiro.si@gmail.com>
2022-12-28 21:30:58 -0300

    Entidade com Embeddable, mapeamento de herança e mapeamento de chaves compostas

--- loja/src/main/java/br/com/alura/loja/modelo/Cliente.java
+++ loja/src/main/java/br/com/alura/loja/modelo/Cliente.java
@@ -1,5 +1,6 @@
 package br.com.alura.loja.modelo;
 
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -14,17 +15,16 @@ public class Cliente {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	private String nome;
-	private String cpf;
+	@Embedded
+	private DadosPessoais dadosPessoais;
 	
 	public Cliente() {
 	}
-	
-	public Cliente(String nome, String cpf) {
-		this.nome = nome;
-		this.cpf = cpf;
-	}
 
+	public Cliente(String nome, String cpf) {
+		this.dadosPessoais = new DadosPessoais(nome, cpf);
+	}
+	
 	public Long getId() {
 		return id;
 	}
@@ -32,21 +32,17 @@ public class Cliente {
 	public void setId(Long id) {
 		this.id = id;
 	}
-
+	
 	public String getNome() {
-		return nome;
+		return this.dadosPessoais.getNome();
 	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
+	
 	public String getCpf() {
-		return cpf;
+		return this.dadosPessoais.getCpf();
 	}
 
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
+	public DadosPessoais getDadosPessoais() {
+		return dadosPessoais;
 	}
 	
 }
